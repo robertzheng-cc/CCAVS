5ecfb5a991ade5a037305f658807b98ce2fb6400
Robert Zheng <robert.zheng@cloudcar.com>
2018-02-23 15:50:30 -0800

    fix caps typo

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = 3000;
 const publicPath = path.join(__dirname, 'public/build');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyparser.json());
+app.use(bodyParser.json());
 app.use(cors());
 
 
